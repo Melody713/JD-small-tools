5b8c6e221dde80689932c5d3ceb02756a7334d3f
0xFFFFFF31 <87599371+0xFFFFFF31@users.noreply.github.com>
2021-08-20 09:46:10 +0800

    Update smiekautoBuild20210819_002Release.TestFixERRBug.sh

--- smiekautoBuild20210819_002Release.TestFixERRBug.sh
+++ smiekautoBuild20210819_002Release.TestFixERRBug.sh
@@ -2,7 +2,7 @@
 #警告！仅在Ubuntu20.04LTS版本进行过测试可以运行，故不能保证在centos，debian，Raspberry Pi OS，red hat等系统上运行
 #警告！仅在x86平台进行测试，故不能保证在arm等平台正常运行
 #警告！严重依赖ql2.8,bash解释器,grep,awk,sed,cd,pwd,echo!!! crontab可有可无
-#警告！shell放到config目录
+#警告！shell放到ql2.8的config目录内,没有映射ql的config，log文件夹的请把本脚本放进ql容器里运行
 #注意！传递参数要定位的log文件夹
 loacl_GPS_log=$1
 
